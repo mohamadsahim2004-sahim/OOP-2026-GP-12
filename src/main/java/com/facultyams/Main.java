0898aeb094713095343f76039243ce1ca1fc1e31
mohamadsahim2004-sahim <mohamadsahim2004@gmail.com>
2026-09-14 18:18:26 +0530

    Create initial shared project structure

---
+++ src/main/java/com/facultyams/Main.java
@@ -0,0 +1,7 @@
+package com.facultyams;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Faculty Academic Management System started successfully!");
+    }
+}
